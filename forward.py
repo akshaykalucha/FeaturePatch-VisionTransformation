a51cf821d4b85edce43be45c575bd2c6421bf31c
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-03-11 23:51:20 +0530

    resize warning

--- forward.py
+++ forward.py
@@ -12,6 +12,16 @@ imagenet_labels = dict(enumerate(open("classes.txt")))
 model = torch.load("model.pth")
 model.eval()
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def computation(image):
     inp = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).to(torch.float32)
@@ -31,3 +41,12 @@ ap.add_argument("-i", "--image", required=True,
 	help="name of the image in base dir")
 args = vars(ap.parse_args())
 
+im = Image.open(args["image"])
+if im.size == (384, 384):
+    print(f"{bcolors.OKGREEN}Running: the image matches the path, running MLP...{bcolors.ENDC}")
+else:
+    print(f"{bcolors.WARNING}Warning: Image not 384x384, resizing and overriding!-{bcolors.ENDC}")
+    resize(args["image"])
+    im = Image.open(args["image"])
+img = (np.array(im) / 128) - 1
+computation(img)
